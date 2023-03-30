9f9bcb69dca65f316c60e070ffe05dfafafe0e0e
Saurabh SIngh <saurabhsinghup@gmail.com>
2023-03-31 04:24:41 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,14 @@
+function makeCall() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var date = document.getElementById("date").value;
+    var time = document.getElementById("time").value;
+  
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Phone Number: " + phone);
+    console.log("Date: " + date);
+    console.log("Time: " + time);
+  }
+  
